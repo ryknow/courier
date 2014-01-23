b0be47aee763e0d7325e71e5401aefe95f1d87a8
ryknow <ryanknowles@gmail.com>
2014-01-23 14:23:50 -0500

    Update CourierClient.scala

--- src/main/scala/org/ryknow/courier/CourierClient.scala
+++ src/main/scala/org/ryknow/courier/CourierClient.scala
@@ -1,10 +1,28 @@
-package main.scala.org.ryknow.courier
+package org.ryknow.courier
 
-import java.net.{SocketAddress, Socket}
+import java.nio._
+import java.nio.channels._
+import java.net._
 
-class CourierClient {
+class CourierClient(host: String, port: Int) {
+  val socketChannel: SocketChannel = new SocketChannel
+  val socketAddress: SocketAddress = new InetSocketAddress(host, port)
+  val byteBuffer: ByteBuffer = ByteBuffer.allocate(64 * 1024)
+  val readBB: ByteBuffer = ByteBuffer.allocate(64 * 1024)
 
-  def connect = {
-    val socket = new Socket()
+  def connect {
+    val connectFrame: String = "CONNECT\naccept-version:1.0,1.1,1.2\nhost:" + host + "\n\n\0"
+    val byteArray: Array[Byte] = connectFrame.toCharArray.map { c => c.asInstanceOf[Byte] }
+    
+    byteBuffer.put(byteArray)
+    
+    socketChannel.connect(socketAddress)
+    socketChannel.write(byteBuffer)
+    
+    socketChannel.read(readBB)
+  }
+  
+  def close {
+    socketChannel.close
   }
 }
